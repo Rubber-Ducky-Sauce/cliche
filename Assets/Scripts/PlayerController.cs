f3d3440a98a279d75641953442bace84ff701df9
Mark Trepanier <marktrepanier.cajigas@gmail.com>
2022-11-03 15:47:12 -0400

    adds hiding boolean on pressing up

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,9 +6,12 @@ public class PlayerController : Actor
 {
     private new Rigidbody2D rigidbody;
     [SerializeField]private float jumpForce = 10f;
+    public bool hiding = false;
+
     private bool isOnGround;
-    [SerializeField]private float groundRay;
+    [SerializeField]private float groundRay = 1f;
     private LayerMask groundLayer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +25,9 @@ public class PlayerController : Actor
     {
         Move();
         Jump();
+        Hide();
+        if (hiding)
+            Debug.Log("hiding");
     }
 
     private void FixedUpdate()
@@ -36,12 +42,18 @@ public class PlayerController : Actor
     public override void Move()
     {
         float horizontal = Input.GetAxis("Horizontal");
+        if(!hiding)
         transform.Translate(Vector3.left * -horizontal * Time.deltaTime * speed);
     }
 
     private void Jump()
     {
-        if (Input.GetButtonDown("Jump") && isOnGround)
+        if (Input.GetButtonDown("Jump") && isOnGround && !hiding)
             rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+    private void Hide()
+    {
+        _ = Input.GetAxis("Vertical") > 0 ? hiding = true : hiding = false;
     }
 }
