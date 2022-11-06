01279299118628e2258db373def08842c139a994
Mark Trepanier <marktrepanier.cajigas@gmail.com>
2022-11-06 05:43:21 -0500

    adds canvas to show locked state

---
+++ Assets/Scripts/TestingScripts/LocksCanvas.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LocksCanvas : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI InfoText;
+    bool keyAcquired = false;
+    private void Update()
+    {
+        GrabKey();
+        TryDoor();
+    }
+
+    void GrabKey()
+    {
+        if(!keyAcquired && GameManager.Instance.activeKey != "")
+        {
+            keyAcquired = true;
+            InfoText.text = "KEY ACQUIRED";
+        }
+    }
+
+    void TryDoor()
+    {
+        if(Input.GetKeyDown(KeyCode.E) && GameManager.Instance.currentLock != null)
+        {
+           _=keyAcquired? InfoText.text = "Door Unlocked!!": InfoText.text = "requires key";
+        }
+    }
+}
