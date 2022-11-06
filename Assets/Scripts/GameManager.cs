0ce21ea2bccb60019cd663413e002700aa23779e
Mark Trepanier <marktrepanier.cajigas@gmail.com>
2022-11-06 04:15:20 -0500

    lets key provide name to gameManager, then sets inactive

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,6 +7,9 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
     [SerializeField] private float loadTime = 2f;
+
+    private string m_activeKey = null;
+    public string activeKey { get { return m_activeKey; } private set { m_activeKey = value; } }
     private void Awake()
     {
         if (Instance != null)
@@ -21,5 +24,10 @@ public class GameManager : MonoBehaviour
     {
         yield return new WaitForSeconds(loadTime);
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void SetKey(string keyName)
+    {
+        activeKey = keyName;
     }
 }
