7b60af10a90615d5035a7c2eca527c4446b7f3ab
Mark Trepanier <marktrepanier.cajigas@gmail.com>
2022-11-07 16:38:28 -0500

    sets spriterender sprtie to canvas item box of collected item

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public abstract class Collectable : MonoBehaviour
 {
+    SpriteRenderer spriteRenderer;
+    Image itemBoxItem;
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Collect();
@@ -12,6 +19,10 @@ public abstract class Collectable : MonoBehaviour
     public virtual void Collect()
     {
         GameManager.Instance.currentCollectable = this;
+        itemBoxItem = GameObject.Find("Item").GetComponent<Image>();
+        itemBoxItem.sprite = spriteRenderer.sprite;
+        itemBoxItem.color = spriteRenderer.color;
         gameObject.SetActive(false);
+        
     }
 }
