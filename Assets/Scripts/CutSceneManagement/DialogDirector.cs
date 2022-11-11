356d19718f6eb55a1cdeba8d4e6d96f67145adf7
Mark Trepanier <marktrepanier.cajigas@gmail.com>
2022-11-11 14:58:33 -0500

    adjusts dialog script to be more easily read

--- Assets/Scripts/CutSceneManagement/DialogDirector.cs
+++ Assets/Scripts/CutSceneManagement/DialogDirector.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class DialogDirector : MonoBehaviour
 {
     [SerializeField] Dialog dialog;
+    [SerializeField] Dialog.Line currentLine;
 
     private void Start()
     {
